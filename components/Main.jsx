6539c1c551ea522eee2b40b77c56c3e3d8ed0bc5
Kundan Kumar <57480481+kundank02@users.noreply.github.com>
2024-01-16 11:22:29 +0530

    Update Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -33,7 +33,7 @@ const Main = () => {
           </p>
           <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
             <a
-              href="https://www.linkedin.com/in/kundankumar02/"
+              href="https://www.linkedin.com/in/kundan02/"
               target="_blank"
               rel="noreferrer"
             >
